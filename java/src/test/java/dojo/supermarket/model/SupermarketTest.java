89f139cb460a53b4caf4f642818c35fac3220023
e1k <105858752+e1kfL0ck@users.noreply.github.com>
2025-11-06 18:21:54 +0100

    Add tests for THREE_FOR_TWO, TEN_PERCENT_DISCOUNT, TWO_FOR_AMOUNT, FIVE_FOR_AMOUNT

--- java/src/test/java/dojo/supermarket/model/SupermarketTest.java
+++ java/src/test/java/dojo/supermarket/model/SupermarketTest.java
@@ -13,6 +13,35 @@ class SupermarketTest {
     // Todo: test all kinds of discounts are applied properly
 
     @Test
+    void tenPercentDiscountNoDiscount() {
+        SupermarketCatalog catalog = new FakeCatalog();
+        Product toothbrush = new Product("toothbrush", ProductUnit.EACH);
+        catalog.addProduct(toothbrush, 0.99);
+        Product apples = new Product("apples", ProductUnit.KILO);
+        catalog.addProduct(apples, 1.99);
+
+        Teller teller = new Teller(catalog);
+        teller.addSpecialOffer(SpecialOfferType.TEN_PERCENT_DISCOUNT, toothbrush, 10.0);
+
+        ShoppingCart cart = new ShoppingCart();
+        cart.addItemQuantity(apples, 2.5);
+
+        // ACT
+        Receipt receipt = teller.checksOutArticlesFrom(cart);
+
+        // ASSERT
+        assertEquals(4.975, receipt.getTotalPrice(), 0.01);
+        assertEquals(Collections.emptyList(), receipt.getDiscounts());
+        assertEquals(1, receipt.getItems().size());
+        ReceiptItem receiptItem = receipt.getItems().get(0);
+        assertEquals(apples, receiptItem.getProduct());
+        assertEquals(1.99, receiptItem.getPrice());
+        assertEquals(2.5 * 1.99, receiptItem.getTotalPrice());
+        assertEquals(2.5, receiptItem.getQuantity());
+
+    }
+
+    @Test
     void tenPercentDiscount() {
         SupermarketCatalog catalog = new FakeCatalog();
         Product toothbrush = new Product("toothbrush", ProductUnit.EACH);
@@ -25,21 +54,156 @@ class SupermarketTest {
 
         ShoppingCart cart = new ShoppingCart();
         cart.addItemQuantity(apples, 2.5);
-        
+        cart.addItemQuantity(toothbrush, 3);
+
         // ACT
         Receipt receipt = teller.checksOutArticlesFrom(cart);
 
         // ASSERT
-        assertEquals(4.975, receipt.getTotalPrice(), 0.01);
-        assertEquals(Collections.emptyList(), receipt.getDiscounts());
-        assertEquals(1, receipt.getItems().size());
+        assertEquals(7.648, receipt.getTotalPrice(), 0.01);
+        assertEquals(2, receipt.getItems().size());
+
         ReceiptItem receiptItem = receipt.getItems().get(0);
         assertEquals(apples, receiptItem.getProduct());
         assertEquals(1.99, receiptItem.getPrice());
-        assertEquals(2.5*1.99, receiptItem.getTotalPrice());
+        assertEquals(2.5 * 1.99, receiptItem.getTotalPrice());
         assertEquals(2.5, receiptItem.getQuantity());
+
+        receiptItem = receipt.getItems().get(1);
+        assertEquals(toothbrush, receiptItem.getProduct());
+        assertEquals(0.99, receiptItem.getPrice());
+        assertEquals(2.97, receiptItem.getTotalPrice(), 0.01);
+        assertEquals(3 * 0.99, receiptItem.getQuantity(), 0.1);
 
     }
 
+    @Test
+    void threeForTwoDiscount() {
+        SupermarketCatalog catalog = new FakeCatalog();
+        Product ps5 = new Product("PS5", ProductUnit.EACH);
+        catalog.addProduct(ps5, 200);
 
+        Teller teller = new Teller(catalog);
+        teller.addSpecialOffer(SpecialOfferType.THREE_FOR_TWO, ps5, 500);
+
+        ShoppingCart cart = new ShoppingCart();
+        cart.addItemQuantity(ps5, 3);
+
+        // ACT
+        Receipt receipt = teller.checksOutArticlesFrom(cart);
+
+        // ASSERT
+        assertEquals(400, receipt.getTotalPrice(), 0.01);
+        assertEquals(1, receipt.getItems().size());
+
+        ReceiptItem receiptItem = receipt.getItems().get(0);
+        assertEquals(ps5, receiptItem.getProduct());
+        assertEquals(200, receiptItem.getPrice());
+        assertEquals(600, receiptItem.getTotalPrice());
+        assertEquals(3, receiptItem.getQuantity());
+    }
+
+    @Test
+    void threeForTwoDiscountForProducts() {
+        SupermarketCatalog catalog = new FakeCatalog();
+        Product ps5 = new Product("PS5", ProductUnit.EACH);
+        catalog.addProduct(ps5, 200);
+
+        Teller teller = new Teller(catalog);
+        teller.addSpecialOffer(SpecialOfferType.THREE_FOR_TWO, ps5, 500);
+
+        ShoppingCart cart = new ShoppingCart();
+        cart.addItemQuantity(ps5, 4);
+
+        // ACT
+        Receipt receipt = teller.checksOutArticlesFrom(cart);
+
+        // ASSERT
+        assertEquals(600, receipt.getTotalPrice());
+        assertEquals(1, receipt.getItems().size());
+
+        ReceiptItem receiptItem = receipt.getItems().get(0);
+        assertEquals(ps5, receiptItem.getProduct());
+        assertEquals(200, receiptItem.getPrice());
+        assertEquals(800, receiptItem.getTotalPrice());
+        assertEquals(4, receiptItem.getQuantity());
+    }
+
+    @Test
+    void twoForAmount() {
+        SupermarketCatalog catalog = new FakeCatalog();
+        Product milk = new Product("Milk", ProductUnit.EACH);
+        catalog.addProduct(milk, 1.37);
+
+        Teller teller = new Teller(catalog);
+        teller.addSpecialOffer(SpecialOfferType.TWO_FOR_AMOUNT, milk, 2.37);
+
+        ShoppingCart cart = new ShoppingCart();
+        cart.addItemQuantity(milk, 2);
+
+        // ACT
+        Receipt receipt = teller.checksOutArticlesFrom(cart);
+
+        // ASSERT
+        assertEquals(2.37, receipt.getTotalPrice());
+        assertEquals(1, receipt.getItems().size());
+
+        ReceiptItem receiptItem = receipt.getItems().get(0);
+        assertEquals(milk, receiptItem.getProduct());
+        assertEquals(1.37, receiptItem.getPrice());
+        assertEquals(2.74, receiptItem.getTotalPrice());
+        assertEquals(2, receiptItem.getQuantity());
+    }
+
+    @Test
+    void twoForAmountThreeProduct() {
+        SupermarketCatalog catalog = new FakeCatalog();
+        Product milk = new Product("Milk", ProductUnit.EACH);
+        catalog.addProduct(milk, 1.37);
+
+        Teller teller = new Teller(catalog);
+        teller.addSpecialOffer(SpecialOfferType.TWO_FOR_AMOUNT, milk, 2.37);
+
+        ShoppingCart cart = new ShoppingCart();
+        cart.addItemQuantity(milk, 3);
+
+        // ACT
+        Receipt receipt = teller.checksOutArticlesFrom(cart);
+
+        // ASSERT
+        assertEquals(3.74, receipt.getTotalPrice());
+        assertEquals(1, receipt.getItems().size());
+
+        ReceiptItem receiptItem = receipt.getItems().get(0);
+        assertEquals(milk, receiptItem.getProduct());
+        assertEquals(1.37, receiptItem.getPrice());
+        assertEquals(4.11, receiptItem.getTotalPrice());
+        assertEquals(3, receiptItem.getQuantity());
+    }
+
+    @Test
+    void fiveForAmount() {
+        SupermarketCatalog catalog = new FakeCatalog();
+        Product steak = new Product("Steak", ProductUnit.EACH);
+        catalog.addProduct(steak, 2.42);
+
+        Teller teller = new Teller(catalog);
+        teller.addSpecialOffer(SpecialOfferType.FIVE_FOR_AMOUNT, steak, 10);
+
+        ShoppingCart cart = new ShoppingCart();
+        cart.addItemQuantity(steak, 5);
+
+        // ACT
+        Receipt receipt = teller.checksOutArticlesFrom(cart);
+
+        // ASSERT
+        assertEquals(10, receipt.getTotalPrice());
+        assertEquals(1, receipt.getItems().size());
+
+        ReceiptItem receiptItem = receipt.getItems().get(0);
+        assertEquals(steak, receiptItem.getProduct());
+        assertEquals(2.42, receiptItem.getPrice());
+        assertEquals(12.1, receiptItem.getTotalPrice());
+        assertEquals(5, receiptItem.getQuantity());
+    }
 }
