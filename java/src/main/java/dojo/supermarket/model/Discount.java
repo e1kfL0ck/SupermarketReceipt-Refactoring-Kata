9190e4136e92db47c3ef9ce8bfd5e2ab8ea1c0d8
e1k <105858752+e1kfL0ck@users.noreply.github.com>
2025-11-12 12:01:16 +0100

    Add a new type of discount, maybe it would need an other class

--- java/src/main/java/dojo/supermarket/model/Discount.java
+++ java/src/main/java/dojo/supermarket/model/Discount.java
@@ -1,13 +1,22 @@
 package dojo.supermarket.model;
 
+import java.util.ArrayList;
+
 public class Discount {
 
     private final String description;
     private final double discountAmount;
-    private final Product product;
+    private Product product;
+    private ArrayList<Product> products;
 
     public Discount(Product product, String description, double discountAmount) {
         this.product = product;
+        this.description = description;
+        this.discountAmount = discountAmount;
+    }
+
+    public Discount(ArrayList<Product> products, String description, double discountAmount) {
+        this.products = products;
         this.description = description;
         this.discountAmount = discountAmount;
     }
