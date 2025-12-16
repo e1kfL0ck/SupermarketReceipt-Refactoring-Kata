8c83f267089f9eeab3c5569ce27d396ad71ef0a1
e1k <105858752+e1kfL0ck@users.noreply.github.com>
2025-12-17 00:21:26 +0100

    feat: Create Coupon class

---
+++ java/src/main/java/dojo/supermarket/model/Coupon.java
@@ -0,0 +1,38 @@
+package dojo.supermarket.model;
+
+import java.time.LocalDate;
+
+public class Coupon {
+
+    private final Product product;
+    private final LocalDate validFrom;
+    private final LocalDate validTo;
+    private final int triggerQuantity;
+    private final int discountedQuantity;
+    private final double discountRate; // 0.5 = -50%
+    private boolean used;
+
+    public Coupon(String code,
+                  Product product,
+                  LocalDate validFrom,
+                  LocalDate validTo,
+                  int triggerQuantity,
+                  int discountedQuantity,
+                  double discountRate) {
+        this.product = product;
+        this.validFrom = validFrom;
+        this.validTo = validTo;
+        this.triggerQuantity = triggerQuantity;
+        this.discountedQuantity = discountedQuantity;
+        this.discountRate = discountRate;
+        this.used = false;
+    }
+
+    public void markUsed() { this.used = true; }
+
+    public boolean isValidOn(LocalDate date) {
+        return (date.isEqual(validFrom) || date.isAfter(validFrom))
+                && (date.isEqual(validTo) || date.isBefore(validTo));
+    }
+
+}
