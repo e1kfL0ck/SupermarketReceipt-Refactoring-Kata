83fc4ac595f59934a4c0a8d6a5c3b6fd5e355165
e1k <105858752+e1kfL0ck@users.noreply.github.com>
2025-12-17 23:01:51 +0100

    CI: Add new method to copy object

--- java/src/main/java/dojo/supermarket/model/Customer.java
+++ java/src/main/java/dojo/supermarket/model/Customer.java
@@ -1,6 +1,7 @@
 package dojo.supermarket.model;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Customer {
@@ -11,6 +12,14 @@ public class Customer {
     public Customer(int id, Map<Product, Coupon> coupons) {
         this.id = id;
         this.coupons = coupons;
+    }
+
+    public Customer(Customer other) {
+        this.coupons = new HashMap<>();
+        for (var e : other.coupons.entrySet()) {
+            this.coupons.put(e.getKey(), new Coupon(e.getValue()));
+        }
+
     }
 
     public Coupon getCouponValidity(Product product, LocalDate checkoutDate) {
