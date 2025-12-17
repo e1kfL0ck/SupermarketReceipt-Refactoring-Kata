f53a25781f5bd387468830d37ab3e0cf43adcd74
e1k <105858752+e1kfL0ck@users.noreply.github.com>
2025-12-17 03:01:28 +0100

    feat: Update Recipet to hold the receiptItems

--- java/src/main/java/dojo/supermarket/model/Receipt.java
+++ java/src/main/java/dojo/supermarket/model/Receipt.java
@@ -8,16 +8,21 @@ import java.util.Map;
 public class Receipt {
 
     private List<Discount> discounts = new ArrayList<>();
+    private List<ReceiptItem> items = new ArrayList<>();
     private double totalPrice = 0;
     private double totalDiscounts = 0;
     private double totalPriceAfterDiscount = 0;
+
+    public Receipt(ArrayList<ReceiptItem> items) {
+        this.items = items;
+    }
 
     public void addDiscount(Discount discount) {
         discounts.add(discount);
     }
 
-    public void pay(Collection<ReceiptItem> receiptItems) {
-        for (ReceiptItem item : receiptItems) {
+    public void pay() {
+        for (ReceiptItem item : items) {
             totalPrice += item.getTotalPrice();
         }
         for (Discount discount : discounts) {
