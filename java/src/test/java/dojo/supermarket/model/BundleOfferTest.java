d3251a7ea93c359c45158d6f56347805d5484842
e1k <105858752+e1kfL0ck@users.noreply.github.com>
2025-12-17 23:00:17 +0100

    CI: Remove bundle with KILO

--- java/src/test/java/dojo/supermarket/model/BundleOfferTest.java
+++ java/src/test/java/dojo/supermarket/model/BundleOfferTest.java
@@ -9,11 +9,11 @@ class BundleOfferTest extends BaseSupermarketTest {
     @Test
     void bundleBasic() {
         ShoppingCart cart = new ShoppingCart();
-        CheckoutCounter counter = new CheckoutCounter(offers, cart);
-        cart.addItemInCart(product("apples"), 2.0);
+        CheckoutCounter counter = new CheckoutCounter(offersMap, cart);
+        cart.addItemInCart(product("toothpaste"), 2.0);
         cart.addItemInCart(product("toothbrush"), 2.0);
 
-        counter.checkout();
+        counter.checkout(customer);
 
         assertEquals(0.4, counter.getReceipt().getTotalDiscounts(), 0.01);
         assertEquals(3.6, counter.getReceipt().getTotalPriceAfterDiscount(), 0.01);
