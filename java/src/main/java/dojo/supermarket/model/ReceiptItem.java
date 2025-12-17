c8a57902513e39ec5523f194d7cc4b39c07b792d
e1k <105858752+e1kfL0ck@users.noreply.github.com>
2025-12-17 20:08:54 +0100

    CI: Validate quantity for EACH unit in ReceiptItem constructor

--- java/src/main/java/dojo/supermarket/model/ReceiptItem.java
+++ java/src/main/java/dojo/supermarket/model/ReceiptItem.java
@@ -10,6 +10,9 @@ public class ReceiptItem {
 
     ReceiptItem(Product p, Double quantity) {
         product = p;
+        if (p.getUnit() == ProductUnit.EACH && quantity % 1 != 0) {
+            throw new IllegalArgumentException("Qty for unit EACH must be an integer value.");
+        }
         this.quantity = quantity;
         totalPrice = p.getPrice() * quantity;
     }
