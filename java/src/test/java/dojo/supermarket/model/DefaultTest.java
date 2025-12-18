66118e7f3baab622772af20d21e98e5c47ca4cfd
e1k <105858752+e1kfL0ck@users.noreply.github.com>
2025-12-18 18:33:32 +0100

    CI: Add dummy test

--- java/src/test/java/dojo/supermarket/model/DefaultTest.java
+++ java/src/test/java/dojo/supermarket/model/DefaultTest.java
@@ -2,6 +2,7 @@ package dojo.supermarket.model;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class DefaultTest extends BaseSupermarketTest{
@@ -12,5 +13,21 @@ public class DefaultTest extends BaseSupermarketTest{
         assertThrows(IllegalArgumentException.class, () ->
                 cart.addItemInCart(product("chocolate"), 3.1)
         );    }
+
+    @Test
+    void dumbCheckout() {
+        ShoppingCart cart = new ShoppingCart();
+        cart.addItemInCart(product("chips"), 2.0);
+        cart.addItemInCart(product("apples"), 1.2);
+
+        CheckoutCounter counter = new CheckoutCounter(offersMap, cart);
+        Receipt r = counter.checkout(customer);
+
+        double total = 2*2 + 1.2*1.5;
+
+        assertEquals(total, r.getTotalPrice(), 0.01);
+        assertEquals(0.0, r.getTotalDiscounts(), 0.01);
+        assertEquals(total, r.getTotalPriceAfterDiscount(), 0.01);
+    }
 
 }
