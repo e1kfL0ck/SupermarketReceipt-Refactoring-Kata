d03d8af945bd90a2188da8932cd3d853540fd31e
e1k <105858752+e1kfL0ck@users.noreply.github.com>
2025-12-20 21:23:29 +0100

    CI: Add a new test with multiple bundle elements

--- java/src/test/java/dojo/supermarket/model/BundleOfferTest.java
+++ java/src/test/java/dojo/supermarket/model/BundleOfferTest.java
@@ -18,4 +18,33 @@ class BundleOfferTest extends BaseSupermarketTest {
         assertEquals(0.4, r.getTotalDiscounts(), 0.01);
         assertEquals(3.6, r.getTotalPrice(), 0.01);
     }
+
+    @Test
+    void bundleMultiple() {
+        ShoppingCart cart = new ShoppingCart();
+        CheckoutCounter counter = new CheckoutCounter(offersMap, cart);
+        //bundle 1 * 3
+        cart.addItemToCart(product("toothpaste"), 4.0);
+        cart.addItemToCart(product("toothbrush"), 3.0);
+
+        //bundle 2 * 2
+        cart.addItemToCart(product("milk"), 2.0);
+        cart.addItemToCart(product("ham"), 3.0);
+        cart.addItemToCart(product("bread"), 4.0);
+
+        Receipt r = counter.checkout(customer);
+
+        double bundle1Price = (1.50 + 0.50)*3; // 2 bundles of toothpaste + toothbrush
+        double bundle2Price = (1.37 + 4.00 + 1.50)*2; // 2 bundles of milk + ham + bread
+
+        double totalPrice = (bundle1Price + bundle2Price)*0.9 +
+                // remaining items
+                (1.50) + // toothpaste
+                (4.00) + // ham
+                (1.50)*2; // bread
+
+        assertEquals(2, r.getDiscounts().size());
+        assertEquals(bundle1Price*0.1 + bundle2Price*0.1, r.getTotalDiscounts(), 0.01);
+        assertEquals(totalPrice, r.getTotalPrice(), 0.01);
+    }
 }
