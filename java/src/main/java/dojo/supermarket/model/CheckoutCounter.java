d346d8109744b5287635c48cfa8a5f2803e021b8
e1k <105858752+e1kfL0ck@users.noreply.github.com>
2025-12-20 10:20:01 +0100

    feat: Add Loyalty methods

--- java/src/main/java/dojo/supermarket/model/CheckoutCounter.java
+++ java/src/main/java/dojo/supermarket/model/CheckoutCounter.java
@@ -2,10 +2,12 @@ package dojo.supermarket.model;
 
 import java.util.Map;
 
+//TODO: Revert pom.xml to original state before submission
 public class CheckoutCounter {
     private final Map<Product, Offer> offersMap;
     private final ShoppingCart cart;
     private DiscountEngine engine;
+    private LoyaltyProgram loyalty = new LoyaltyProgram();
 
     public CheckoutCounter(Map<Product, Offer> offersMap, ShoppingCart cart) {
         this.offersMap = offersMap != null ? offersMap : new java.util.HashMap<>();
@@ -14,9 +16,24 @@ public class CheckoutCounter {
     }
 
     public Receipt checkout(Customer customer) {
-        Receipt receipt = new Receipt(cart.items());// IMPORTANT: receipt exist before applying offers and coupons (so qty is the same)
+        Receipt receipt = new Receipt(cart.items());
         engine.applyAll(cart, customer, receipt);
+
+        double total = receipt.computeTotalPrice();
+
+        int maxPointsNeeded = loyalty.moneyToPoints(total);
+        int usedPoints = customer.useCreditPoints(maxPointsNeeded);
+        double covered = loyalty.pointsToMoney(usedPoints);
+
+        double paidByCash = total - covered;
+
+        //receipt.pay(new PaymentResult(paidByCash, usedPoints));
         receipt.pay();
+
+        // gain de points sur l'argent réellement dépensé (règle simple)
+        int earned = loyalty.earnPoints(paidByCash);
+        customer.addCreditPoints(earned);
+
         return receipt;
     }
 }
